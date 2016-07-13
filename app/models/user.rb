6d7cd91e66891166e674bf29afbf2157153d3036
oliverokwaro <oliverokwaro@gmail.com>
2016-07-13 09:40:25 +0300

    Add Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
 validates :name, presence: true, legnth: { maximum:50 }
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 validates :email, presence:true, legnth: { maximum:255 }
-validates :position, presence: true, length: { maximum:50 }
+validates :position, presence: true, length: { maximum:50 },
+           format: { with: VALID_EMAIL_REGEX },
+           uniqueness: { case_sensitive: false }
 end
