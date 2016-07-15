01b0ad429be9ed305b24ae290b9b898417c5d22e
oliverokwaro <oliverokwaro@gmail.com>
2016-07-15 17:34:51 +0300

    Unsucsessful sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
   def show
-  @user = User.find_by(params [:id])
+  @user = User.find(params[:id])
   end
 
   def new 
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #flash
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params 
+    params.require(:user).permit(:name, :email, :position, :password, :password_confirmation)
   end
 end
