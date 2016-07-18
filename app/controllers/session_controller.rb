743a2e5c9c22a4bea144a19a4065eeaaae0e073d
oliverokwaro <oliverokwaro@gmail.com>
2016-07-18 15:26:32 +0300

    Remember Me.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,8 +6,9 @@ class SessionController < ApplicationController
      user = User.find_by(email: params[:session][:email].downcase)
      if user && user.authenticate(params[:session][:password])
     log_in user
+    params[:session][:remember_me] == '1' ? remember(user) : forget(user)
     redirect_to user
-  else
+    else
     flash.now[:danger] = 'Invalid email/password combination.'
     render 'new'
     end
@@ -15,9 +16,9 @@ class SessionController < ApplicationController
 
 
   def destroy
-  log_out
+  log_out if logged_in?
   redirect_to root_url
   end
 
-
+  
 end
