d0a750d81fa350420f7111ad5f2ef3318043777c
oliverokwaro <oliverokwaro@gmail.com>
2016-07-19 16:21:36 +0300

    Faking Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  
+  def index 
+  @user = User.all
+  end
 
   def show
   @user = User.find(params[:id])
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "Example User",
+       email: "example@railstutorial.org",
+       position: "Librarian",
+       password: "foobar",
+       password_confirmation: "foobar")
+
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  position = "position"
+  password = "password"
+  User.create!(name: name,
+              email: email,
+              position: position,
+              password: password,
+              password_confirmation: password)
+end
