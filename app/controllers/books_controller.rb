faf5d997ad09cc14b0ec6f4165e3f2f6f68d7f72
oliverokwaro <oliverokwaro@gmail.com>
2016-07-25 14:29:29 +0300

    Refactor views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:success] = "book created"
+      flash[:success] = "Book Added"
       redirect_to books_url
     else
       render 'new'
@@ -36,17 +36,23 @@ class BooksController < ApplicationController
  @book = Book.find(params[:id])
  if @book.update_attributes(book_params)
    flash[:success] = "Book Updated"
+   redirect_to books_url
  else
    render 'edit'
  end
  end
 
+ def destroy
+ @book = Book.find(params[:id])
+ @book.destroy
+ flash[:success] = "Book is successfully deleted"
+ redirect_to books_url
+ end
 
 
 private
   def book_params
     params.require(:book).permit(:title, :author, :category, :isbn, :publisher)
   end
-
 
 end
